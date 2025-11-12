755a4a83d47f7435b1209e21cbc464f4fa22fb83
dadithya01 <dadithya01@gmail.com>
2025-11-12 08:59:37 +0530

    Add customer and item databases for merchandise management

---
+++ db/db.js
@@ -0,0 +1,12 @@
+//Customer DB
+export let customers_db = [
+    { id: 'C001', name: 'Duvindu Adithya', email: 'adithya@gmail.com', phone: '0716236452', address: 'Panadura' }
+];
+
+//Item DB
+export let items_db = [
+    { code: 'I001', name: 'Red Bull Racing 2025 Team Max Verstappen Driver T-Shirt.', description: 'Show your support for Max Verstappen for every start, every pit stop and every chequered flag of the 2025 race season with this official Red Bull Racing tee. It features his name and car number on the chest, along with iconic Red Bull and Castore emblems and graphics for a team look you can proudly wear anywhere to show your allegiance.', price: 63.00, quantity: 150 },
+    { code: 'I002', name: 'Scuderia Ferrari 2025 Team T-Shirt.', description: 'Take an essential garment and give it a real racing flourish. No item of clothing is likely to accompany you as far and wide as a classic tee, so make sure yours reflects your passion and makes you look good.', price: 57.96, quantity: 15 }
+];
+
+export let orders_db = [];
